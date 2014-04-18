ef070a34ae6403102c8957c399a9d8db9d57a9fc
KunSTYKE02 <KunSTYKE02@SamsungNotebook>
2014-04-18 23:13:25 +0700

    Lab2 ArrayList

---
+++ src/com/lab2/datastructure/TestArrayList.java
@@ -0,0 +1,19 @@
+package com.lab2.datastructure;
+
+public class TestArrayList {
+
+	public static void main(String[] args) {
+
+			
+			ArraryList arr = new ArraryList();
+			arr.add("1");
+			System.out.println(arr);
+			arr.add("2");
+			System.out.println(arr);
+			arr.add("3");
+			System.out.println(arr);
+			arr.add(2, "4");
+			System.out.println(arr);
+	}
+
+}
